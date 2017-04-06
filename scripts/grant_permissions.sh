1e7ccc7fc576bd9be72119ea876157610c159b3d
Alberto Moreta <amoreta@chaotic-kingdoms.com>
2017-04-06 16:52:34 +0200

    Updated grant permissions script

--- scripts/grant_permissions.sh
+++ scripts/grant_permissions.sh
@@ -1,17 +1,23 @@
+// grant_permissions.sh
 #!/bin/bash
 #
-# Usage
-# You can run any command adb provides on all your currently connected devices
-# ./adb+ <command> is the equivalent of ./adb -s <serial number> <command>
-#
+# argument: apk adb package
+# ex: sh grant_permissions.sh <path_to_adb> <package>
 
-adb devices | while read line
-do
+adb=$1
+package=$2
 
-    if [ ! "$line" = "" ] && [ `echo $line | awk '{print $2}'` = "device" ]
-    then
-        device=`echo $line | awk '{print $1}'`
-        echo "$device $@ ..."
-        adb -s $device $@
-    fi
+if [ "$#" = 0 ]; then
+    echo "No parameters found, run with sh grant_permissions.sh <path_to_adb> <package>"
+    exit 0
+fi
+
+# get all the devices
+devices=$($adb devices | grep -v 'List of devices' | cut -f1 | grep '.')
+
+for device in $devices; do
+    echo "Setting permissions to device" $device "for package" $package
+    $adb -s $device shell pm grant $package android.permission.READ_EXTERNAL_STORAGE
+    $adb -s $device shell pm grant $package android.permission.WRITE_EXTERNAL_STORAGE
+    $adb -s $device shell pm grant $package android.permission.SET_ANIMATION_SCALE
 done
