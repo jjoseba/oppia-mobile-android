e0d386f045167b00dc3d7d19669b0b7b6071fd23
Alberto Moreta <amoreta@chaotic-kingdoms.com>
2017-04-21 14:22:42 +0200

    Allow grant_permissions in more than one device for windows script

--- scripts/grant_permissions.bat
+++ scripts/grant_permissions.bat
@@ -1,3 +1,7 @@
+
+:argument: apk adb package
+:ex: sh grant_permissions.sh <path_to_adb> <package>
+
 set adb=%1
 set package=%2
 
@@ -6,14 +10,22 @@ if "%2"=="" (
 	exit 
 )
 
-"%adb%" devices | findstr /v "List of devices"> test2.txt
-set /p devices=<test2.txt
-del test2.txt 
-echo %devices%
+"%adb%" devices | findstr /v "List of devices"> devices.txt
 
-FOR /f %%i IN ("%devices%") DO (
-	echo Setting permissions to device %%i for package %package% 
-    "%adb%" -s %%i shell pm grant %package% android.permission.READ_PHONE_STATE
-    "%adb%" -s %%i shell pm grant %package% android.permission.WRITE_EXTERNAL_STORAGE
-    "%adb%" -s %%i shell pm grant %package% android.permission.SET_ANIMATION_SCALE
+setlocal EnableDelayedExpansion
+
+set i=0
+FOR /F %%a IN (devices.txt) DO (
+    set /A i+=1
+    set devices[!i!]=%%a
+    echo !devices[%%i]!
+)
+del devices.txt
+set n=%i%
+
+FOR /L %%i IN (1,1,%n%) DO (
+	echo Setting permissions to device !devices[%%i]! for package %package%
+    "%adb%" -s !devices[%%i]! shell pm grant %package% android.permission.READ_PHONE_STATE
+    "%adb%" -s !devices[%%i]! shell pm grant %package% android.permission.WRITE_EXTERNAL_STORAGE
+    "%adb%" -s !devices[%%i]! shell pm grant %package% android.permission.SET_ANIMATION_SCALE
 )	
