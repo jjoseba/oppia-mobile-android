fddada2e8fc6228e5449f64176d60c80553a72e3
Alberto Moreta <amoreta@chaotic-kingdoms.com>
2017-04-21 14:22:06 +0200

    Changes on grant_permissions linux script

--- scripts/grant_permissions.sh
+++ scripts/grant_permissions.sh
@@ -1,8 +1,8 @@
+#!/usr/bin/env bash
 // grant_permissions.sh
-#!/bin/bash
 #
-# argument: apk adb package
 # ex: sh grant_permissions.sh <path_to_adb> <package>
+#
 
 adb=$1
 package=$2
@@ -12,11 +12,11 @@ if [ "$#" = 0 ]; then
     exit 0
 fi
 
-# get all the devices
+# get all the connected devices
 devices=$($adb devices | grep -v 'List of devices' | cut -f1 | grep '.')
 
 for device in $devices; do
-    echo "Setting permissions to device" $device "for package" $package
+    echo "Setting permissions to device " $device " for package " $package
     $adb -s $device shell pm grant $package android.permission.READ_PHONE_STATE
     $adb -s $device shell pm grant $package android.permission.WRITE_EXTERNAL_STORAGE
     $adb -s $device shell pm grant $package android.permission.SET_ANIMATION_SCALE
